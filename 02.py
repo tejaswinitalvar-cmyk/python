53d22210eeaf3d05b6d0487b92357bdef8591f85
Tejaswini T <tejaswinitalvar@gmail.com>
2026-05-26 21:11:17 +0530

    Add simple calculator program in Python

---
+++ 02.py
@@ -0,0 +1,8 @@
+# simple calculator program using python programming language
+a = int(input("Enter first number: "))    
+b = int(input("Enter second number: "))
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
