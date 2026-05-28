290eaf4ca11edd0a9b10580fdb4e08e8d5811e16
Tejaswini T <tejaswinitalvar@gmail.com>
2026-05-28 23:38:14 +0530

    Add simple number swapping program in Python

---
+++ 04.py
@@ -0,0 +1,11 @@
+# Simple swapping of two numbers program using python programming language
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+temp = a
+a = b
+b = temp
+
+print("After swapping:")
+print("First number =", a)
+print("Second number =", b)
