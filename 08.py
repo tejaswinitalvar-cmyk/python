d8d3320a7d5b05c08e1d9d79953e7a3fdcd1f796
Tejaswini T <tejaswinitalvar@gmail.com>
2026-06-01 22:40:49 +0530

    Add program to find largest of two numbers
    Implement a program to find the largest of two numbers.

---
+++ 08.py
@@ -0,0 +1,9 @@
+# Simple program to find largest of two numbers using python programming language
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print("Largest number is:", a)
+else:
+    print("Largest number is:", b)
