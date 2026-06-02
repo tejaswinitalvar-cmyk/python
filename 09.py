e51a7fb30611288966db783f4d473d16195cde5e
Tejaswini T <tejaswinitalvar@gmail.com>
2026-06-02 22:49:52 +0530

    Add program to calculate age difference between two names

---
+++ 09.py
@@ -0,0 +1,11 @@
+# Learning variables and f-strings using python progamming language
+
+boy_name = input("Boy Name: ")
+boy_age = int(input("Boy Age: "))
+
+girl_name = input("Girl Name: ")
+girl_age = int(input("Girl Age: "))
+
+age_diff = boy_age - girl_age
+
+print(f"{boy_name} loves {girl_name}. Age difference is {age_diff}")
