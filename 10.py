318f2db191a1ac3c9364d95b08a26bc3657eeef5
Tejaswini T <tejaswinitalvar@gmail.com>
2026-06-03 22:30:33 +0530

    Add Even or Odd Number Checker in Python

---
+++ 10.py
@@ -0,0 +1,8 @@
+# Even or Odd Number Checker using python programming language
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print(num, "is Even")
+else:
+    print(num, "is Odd")
