c50e6fc1d84ce0959778b9acfbbc2eec4a70aaf6
Tejaswini T <tejaswinitalvar@gmail.com>
2026-06-13 15:49:19 +0530

    Add script to remove duplicates from a list
    This script removes duplicates from a list and prints the original and unique lists.

---
+++ 19.py
@@ -0,0 +1,8 @@
+# Remove duplicates from a list using python programming language 
+
+numbers = [1, 2, 2, 3, 4, 4, 5]
+
+unique_numbers = list(set(numbers))
+
+print("Original List:", numbers)
+print("List without duplicates:", unique_numbers)
