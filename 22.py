d84208d87f43f55437923684cd288592adfb83e2
Tejaswini T <tejaswinitalvar@gmail.com>
2026-06-16 00:02:50 +0530

    Add number classification script in 22.py
    Implement a program to check if a number is positive, negative, or zero.

---
+++ 22.py
@@ -0,0 +1,10 @@
+# Check positive, negative, or zero using python programming language 
+
+num = float(input("Enter a number: "))
+
+if num > 0:
+    print("Positive Number")
+elif num < 0:
+    print("Negative Number")
+else:
+    print("Zero")
