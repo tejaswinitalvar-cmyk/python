92e7775c1ff31516c9704d69c6ca0f57b3443496
Tejaswini T <tejaswinitalvar@gmail.com>
2026-06-19 00:12:54 +0530

    Add character frequency counter in 25.py

---
+++ 25.py
@@ -0,0 +1,8 @@
+# Count occurrences of a character in a string using python programming language 
+
+text = input("Enter a string: ")
+char = input("Enter a character: ")
+
+count = text.count(char)
+
+print("Frequency of", char, "=", count)
