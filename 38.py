bd60645b5cebd034d260003d56ba8a5bd9ffc6a2
Tejaswini T <tejaswinitalvar@gmail.com>
2026-07-02 00:11:49 +0530

    Add script to check if a list is sorted

---
+++ 38.py
@@ -0,0 +1,8 @@
+# Check whether a list is sorted using python programming language 
+
+numbers = [10, 20, 30, 40, 50]
+
+if numbers == sorted(numbers):
+    print("The list is sorted.")
+else:
+    print("The list is not sorted.")
