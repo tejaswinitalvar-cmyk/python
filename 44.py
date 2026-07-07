686da47262e4a000959859625665f17babbd510f
Tejaswini T <tejaswinitalvar@gmail.com>
2026-07-08 00:05:55 +0530

    Add equality check for two lists in 44.py

---
+++ 44.py
@@ -0,0 +1,9 @@
+# Check whether two lists are equal using python programming language 
+
+list1 = [10, 20, 30, 40]
+list2 = [10, 20, 30, 40]
+
+if list1 == list2:
+    print("The lists are equal.")
+else:
+    print("The lists are not equal.")
