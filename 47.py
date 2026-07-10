e1fac4c3f4c6ad4d1ae2fa7860b1b2865aa9d449
Tejaswini T <tejaswinitalvar@gmail.com>
2026-07-11 00:51:45 +0530

    Add check for empty list in 47.py

---
+++ 47.py
@@ -0,0 +1,8 @@
+# Check whether a list is empty using python programming language 
+
+numbers = []
+
+if len(numbers) == 0:
+    print("The list is empty.")
+else:
+    print("The list is not empty.")
