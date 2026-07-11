c8c57a3d0ab668bcac0f1a4e6c5ff4f01bc995b7
Tejaswini T <tejaswinitalvar@gmail.com>
2026-07-12 00:35:08 +0530

    Add script to find max and min in a list

---
+++ 48.py
@@ -0,0 +1,6 @@
+# Find the maximum and minimum number in a list using python programming language 
+
+numbers = [15, 8, 23, 42, 4, 16]
+
+print("Maximum number =", max(numbers))
+print("Minimum number =", min(numbers))
