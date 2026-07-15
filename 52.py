74f7242915a1867e822ab365892b2eb646106812
Tejaswini T <tejaswinitalvar@gmail.com>
2026-07-16 00:07:58 +0530

    Add digit-checking script in 52.py

---
+++ 52.py
@@ -0,0 +1,8 @@
+# Check whether a string contains only digits using python programming language 
+
+text = input("Enter a string: ")
+
+if text.isdigit():
+    print("The string contains only digits.")
+else:
+    print("The string does not contain only digits.")
