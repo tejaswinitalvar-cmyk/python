15ac840854cb0780b035db18f5a57035b414bbbf
Tejaswini T <tejaswinitalvar@gmail.com>
2026-07-29 00:55:43 +0530

    Add length comparison for two lists in Python

---
+++ 65.py
@@ -0,0 +1,9 @@
+# Check whether two lists have the same length using python programming language 
+
+list1 = [10, 20, 30, 40]
+list2 = [1, 2, 3, 4]
+
+if len(list1) == len(list2):
+    print("Both lists have the same length.")
+else:
+    print("The lists have different lengths.")
