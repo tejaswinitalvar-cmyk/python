7c52ccf9c8306b3307c64fe64afdb79eea58df74
Tejaswini T <tejaswinitalvar@gmail.com>
2026-08-04 18:42:37 +0530

    Add script to calculate product of list elements

---
+++ 71.py
@@ -0,0 +1,10 @@
+# Find the product of all elements in a list using python programming language
+
+numbers = [2, 3, 4, 5]
+
+product = 1
+
+for num in numbers:
+    product *= num
+
+print("Product of all elements =", product)
