c0fb74672f4696d014006cecc22e7f941fb61dc8
Tejaswini T <tejaswinitalvar@gmail.com>
2026-08-13 17:08:51 +0530

    Add script to find numbers greater than average
    This script calculates the average of a list of numbers and prints those that are greater than the average.

---
+++ 80.py
@@ -0,0 +1,12 @@
+# Find numbers greater than the average using python programming language 
+
+numbers = [10, 20, 30, 40, 50]
+
+average = sum(numbers) / len(numbers)
+
+print("Average =", average)
+print("Numbers greater than average:")
+
+for num in numbers:
+    if num > average:
+        print(num)
