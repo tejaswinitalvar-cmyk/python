d525e535fe89fb813e943d1432bd9e82c79b7161
Tejaswini T <tejaswinitalvar@gmail.com>
2026-08-14 11:29:06 +0530

    Add script to find numbers less than the average
    This script calculates the average of a list of numbers and prints the numbers that are less than the average.

---
+++ 81.py
@@ -0,0 +1,12 @@
+# Find numbers less than the average using python programming language 
+
+numbers = [10, 20, 30, 40, 50]
+
+average = sum(numbers) / len(numbers)
+
+print("Average =", average)
+print("Numbers less than average:")
+
+for num in numbers:
+    if num < average:
+        print(num)
