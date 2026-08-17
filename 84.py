6aba35529d50cfa8c7f49a98eb4660d81f1daea2
Tejaswini T <tejaswinitalvar@gmail.com>
2026-08-17 10:27:15 +0530

    Add script to find first number greater than 50

---
+++ 84.py
@@ -0,0 +1,8 @@
+# Find the first number greater than 50 using python programming language 
+
+numbers = [12, 25, 48, 55, 72, 30]
+
+for num in numbers:
+    if num > 50:
+        print("First number greater than 50 =", num)
+        break
