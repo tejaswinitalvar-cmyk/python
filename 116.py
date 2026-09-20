d08aec59a3511adbfda4204ea52e4028beb74c9f
Tejaswini T <tejaswinitalvar@gmail.com>
2026-09-20 08:35:15 +0530

    Add Duck Number checker in 116.py

---
+++ 116.py
@@ -0,0 +1,8 @@
+# Check whether a number is a Duck Number using python programming language 
+
+num = input("Enter a number: ")
+
+if "0" in num[1:]:
+    print("It is a Duck Number.")
+else:
+    print("It is not a Duck Number.")
